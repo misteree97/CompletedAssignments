a096829317f37f4b25353afc17ccaf13f5ca65f5
Elliot Lohr <elliot-lohr@uiowa.edu>
2017-09-11 09:22:04 -0500

    20170911 Quiz

--- oral_exam1/6-10_RoundNum_Easy/src/RoundNum.java
+++ oral_exam1/6-10_RoundNum_Easy/src/RoundNum.java
@@ -1,8 +1,46 @@
+/**
+ * Programmer: Elliot Lohr
+ * Date: 09/07/17
+ * Description: Program that rounds a user input double to an integer, as well as tens, hundedths and thousandths place
+ */
+
+import java.util.Scanner;
+
 public class RoundNum {
 
-
-    public int RoundToInteger(double number)
+    public static void main(String[] args)
     {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number with at least 3 decimal places to round");
+        double unroundedNumber = sc.nextDouble();
+        double tenths =RoundToTenths(unroundedNumber);
+        double hundredths = RoundToHundredths(unroundedNumber);
+        double thousandths = RoundToThousandths(unroundedNumber);
+        int integer = RoundToInteger(unroundedNumber);
+        System.out.println("Rounded to Thousandths: " + thousandths);
+        System.out.println("Rounded to Hundredths: " + hundredths);
+        System.out.println("Rounded to Tenths: " + tenths);
+        System.out.println("Rounded to Integer: " + integer);
+    }
 
+    public static double RoundToTenths(double number)
+    {
+        double y = Math.floor((number*10)+.5)/10;
+        return y;
+    }
+    public static double RoundToHundredths(double number)
+    {
+        double y = Math.floor((number*100)+.5)/100;
+        return y;
+    }
+    public static double RoundToThousandths(double number)
+    {
+        double y = Math.floor((number*1000)+.5)/1000;
+        return y;
+    }
+    public static int RoundToInteger(double number)
+    {
+        int y =(int)Math.floor((number*1)+.5)/1;
+        return y;
     }
 }
