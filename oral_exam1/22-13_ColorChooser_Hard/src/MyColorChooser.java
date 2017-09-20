55d790384e30ef035990d31cef5418624f8db869
Elliot Lohr <elliot-lohr@uiowa.edu>
2017-09-20 09:08:31 -0500

    22-13_ColorChooser_Har

---
+++ oral_exam1/22-13_ColorChooser_Hard/src/MyColorChooser.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+import java.util.*;
+public class MyColorChooser extends JFrame {
+    JSlider redOption, greenOption, blueOption;
+    JLabel redLabel, greenLabel, blueLabel;
+    JPanel panel; 
+}
