fb989bb690ed029123185fa604206808b6d13f95
Elliot Lohr <elliot-lohr@uiowa.edu>
2017-09-21 10:03:30 -0500

    Cleaning up folder

--- oral_exam1/S1_Computus_Medium/src/ComputusTester.java
+++ oral_exam1/S1_Computus_Medium/src/ComputusTester.java
@@ -11,5 +11,6 @@ public class ComputusTester {
         System.out.println("Month: " + c.getMonth() + " Day: " + c.getDay());
         System.out.println("MEDIUM PART");
         c.DisplayEasterDates();
+        System.out.println("");
     }
 }
