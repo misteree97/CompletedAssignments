4be225500f689becdc231ec990c2ed4dc713b1c1
Elliot Lohr <elliot-lohr@uiowa.edu>
2017-09-21 11:29:11 -0500

    Revert "More small changes"
    This reverts commit 76a5b47f8c35344ad057193f64502deeda1bb7c9.

--- oral_exam1/22-13_ColorChooser_Hard/src/MyColorChooser.java
+++ oral_exam1/22-13_ColorChooser_Hard/src/MyColorChooser.java
@@ -1,41 +1,12 @@
 import javax.swing.*;
-import java.awt.*;
 import java.util.*;
 public class MyColorChooser extends JFrame {
     JSlider redOption, greenOption, blueOption;
     JLabel redLabel, greenLabel, blueLabel;
-    JPanel panel = new JPanel();
-    JTextField tf = new JTextField(25);
+    JPanel panel;
 
     public MyColorChooser()
     {
-       // redOption = new JSlider(0,255, 0);
-       // greenOption = new JSlider(0,255,0);
-       // blueOption = new JSlider(0,255,0);
-
-
-
-        //event e = new event();
-        //redOption.addChangeListener(e);
-       // greenOption.addChangeListener(e);
-      //  blueOption.addChangeListener(e);
-
-       // panel = new JPanel();
-      //  panel.setBackground(Color.BLACK);
-        setTitle("MyColorChooser");
-        setVisible(true);
-        setSize(300,300);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        panel.add(tf);
-        add(panel);
-
-
-
-
-
-    }
-    public static void main(String[] args)
-    {
-        MyColorChooser a = new MyColorChooser();
+        redOption = new JSlider()
     }
 }
