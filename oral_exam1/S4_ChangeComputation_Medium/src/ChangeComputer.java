2afed1903a6acb0bbf38cf2413ce6656167eecc4
Elliot Lohr <elliot-lohr@uiowa.edu>
2017-09-26 17:02:56 -0500

    Changecomputer926

--- oral_exam1/S4_ChangeComputation_Medium/src/ChangeComputer.java
+++ oral_exam1/S4_ChangeComputation_Medium/src/ChangeComputer.java
@@ -1,20 +1,31 @@
 import java.lang.*;
-public class ChangeComputer {
-    //private float totalAmount;
-    //private float customerAmount;
-    private int twentys = ((int)(Math.random() * 15) + 1);
-    private int tens = ((int)(Math.random() * 15) + 1);
-    private int fives = ((int)(Math.random() * 15) + 1);
-    private int ones = ((int)(Math.random() * 15) + 1);
-    private int quarters = ((int)(Math.random() * 15) + 1);
-    private int dimes = ((int)(Math.random() * 15) + 1);
-    private int nickels = ((int)(Math.random() * 15) + 1);
-    private int pennys = ((int)(Math.random() * 15) + 1);
+import java.util.*;
+public class ChangeComputer
+{
+    private double totalAmount;
+    private double customerAmount;
+    private int twentys = ((int)(Math.random() * 15));
+    private int tens = ((int)(Math.random() * 15));
+    private int fives = ((int)(Math.random() * 15));
+    private int ones = ((int)(Math.random() * 15));
+    private int quarters = ((int)(Math.random() * 15));
+    private int dimes = ((int)(Math.random() * 15));
+    private int nickels = ((int)(Math.random() * 15));
+    private int pennys = ((int)(Math.random() * 15));
 
 
     public ChangeComputer()
     {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter the price of the item");
+        totalAmount = sc.nextDouble();
+        System.out.println("Enter the amount paid");
+        customerAmount = sc.nextDouble();
+        while(customerAmount < totalAmount)
+        {
+            System.out.println("You must enter an amount paid that is > the price of the item. Please enter amount paid again.");
+            customerAmount = sc.nextDouble();
+        }
     }
     public void displayCashRegister()
     {
@@ -29,88 +40,80 @@ public class ChangeComputer {
         System.out.println("Pennys: " + pennys);
         System.out.println("");
     }
-    public void payForItem(double itemPrice, double customerPaid)
+    public void payForItem()
     {
         if(twentys > 0)
         {
-            int twentysDivisable = ((int)itemPrice/20);
+            int twentysDivisable = ((int)totalAmount/20);
             while(twentysDivisable >twentys  && twentys > 0)
             {
-                itemPrice = itemPrice % twentysDivisable;
+                totalAmount = totalAmount % twentysDivisable;
                 twentys--;
             }
         }
         if(tens > 0 )
         {
-            int tensDivisable = ((int) itemPrice / 10);
+            int tensDivisable = ((int) totalAmount / 10);
             while(tensDivisable > tens && tens > 0)
             {
-                itemPrice = itemPrice % tensDivisable;
+                totalAmount = totalAmount % tensDivisable;
                 tens--;
             }
         }
         if(fives > 0)
         {
-            int fivesDivisable = ((int) itemPrice / 5);
+            int fivesDivisable = ((int) totalAmount / 5);
             while(fivesDivisable > fives && fives > 0)
             {
-                itemPrice = itemPrice % fivesDivisable;
+                totalAmount = totalAmount % fivesDivisable;
                 fives--;
             }
         }
         if(ones > 0)
         {
-            int onesDivisable = (int)itemPrice;
+            int onesDivisable = (int)totalAmount;
             while(ones > onesDivisable && ones > 0)
             {
-                itemPrice = itemPrice % onesDivisable;
+                totalAmount = totalAmount % onesDivisable;
                 ones--;
             }
         }
         if(quarters > 0)
         {
-            int quartersDivisable = (int)( itemPrice / .25);
+            int quartersDivisable = (int)(totalAmount / .25);
             while(quarters > quartersDivisable && quarters > 0)
             {
-                itemPrice = itemPrice % quartersDivisable;
+                totalAmount = totalAmount % quartersDivisable;
                 quarters--;
             }
         }
         if(dimes > 0)
         {
-            int dimesDivisable = (int)(itemPrice / .1);
+            int dimesDivisable = (int)(totalAmount / .1);
             while(dimes > dimesDivisable && dimes > 0)
             {
-                itemPrice = itemPrice % dimesDivisable;
+                totalAmount = totalAmount % dimesDivisable;
                 dimes--;
             }
         }
         if(nickels > 0)
         {
-            int nickelsDivisable = (int)(itemPrice / .05);
+            int nickelsDivisable = (int)(totalAmount / .05);
             while(fives > nickelsDivisable && fives > 0)
             {
-                itemPrice = itemPrice % nickelsDivisable;
+                totalAmount= totalAmount % nickelsDivisable;
                 nickels--;
             }
         }
         if(pennys > 0)
         {
-            int pennysDivisable = (int)(itemPrice / .01);
+            int pennysDivisable = (int)(totalAmount / .01);
             while(pennys > pennysDivisable && pennys > 0)
             {
-                itemPrice = itemPrice % pennysDivisable;
+                totalAmount = totalAmount % pennysDivisable;
                 pennys--;
             }
         }
-
-
+        displayCashRegister();
     }
-
-    public void cashRegister()
-    {
-
-    }
-
-
 }
