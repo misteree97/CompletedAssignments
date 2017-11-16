658cbc92f3776bb8f6e92bcea4ce7685fcff0d04
Elliot Lohr <elliot-lohr@uiowa.edu>
2017-11-16 12:03:04 -0600

    Maze Traversal almost complete

--- oral_exam2/18-20_MazeTraversal_Hard/src/MazeTraverse.java
+++ oral_exam2/18-20_MazeTraversal_Hard/src/MazeTraverse.java
@@ -4,6 +4,8 @@ public class MazeTraverse {
     static char maze[][];
     static Scanner sc = new Scanner(System.in);
     static int move = 0;
+    static final int startX = 2;
+    static final int startY = 0;
 
     public boolean mazeTraversal(char maze2[][], int x, int y)
     {
@@ -11,11 +13,93 @@ public class MazeTraverse {
         move++;
 
         printMaze();
-    }
-    public void traverse()
-    {
 
+        if(mazeExited(x, y) && move > 1)
+        {
+            System.out.println("Maze has been exited");
+        }
+        else if(x == startX && y == startY)
+        {
+            System.out.println("Back at Start");
+        }
+        else
+        {
+            System.out.println("Enter 1 to continue or 2 to exit");
+            int kontinue = sc.nextInt();
+            if(kontinue == 1)
+            {
+                for(int i = 0; i < 4; i++)
+                {
+                   if(validMove(x+1, y))
+                    {
+                        if (mazeTraversal(maze2, x, y + 1))
+                        {
+                            return true;
+                        }
+                        else if(validMove(x - 1, y))
+                        {
+                            if (mazeTraversal(maze2, x - 1, y))
+                            {
+                                return true;
+                            }
+                        }
+                        else if(validMove(x, y+1))
+                        {
+                            if(mazeTraversal(maze2, x, y+1))
+                            {
+                                return true;
+                            }
+                        }
+                        else if(validMove(x, y -1))
+                        {
+                            if(mazeTraversal(maze2, x, y -1))
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+            else if(kontinue == 2)
+            {
+                System.exit(0);
+            }
+
+
+        }
     }
+    public static boolean validMove(int row, int column)
+    {
+        if (row >= 0 && row <= 11 && column >= 0 && column <= 11 && maze[row][column] == '.')
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public void checkTraverse()
+    {
+        boolean canTraverse = mazeTraversal(maze, startX, startY);
+        if(!canTraverse)
+        {
+            System.out.println("No Path to complete maze");
+            System.exit(0);
+        }
+    }
+    public static boolean mazeExited(int row, int column)
+    {
+        if(row == 0 || row == 11 || column == 0 || column == 11)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
     public void printMaze()
     {
         int x = 5;
@@ -25,7 +109,7 @@ public class MazeTraverse {
         {
             for(int j = 0; j < maze[i].length; j++)
             {
-                if(maze[i][j] == "0")
+                if(maze[i][j] == '0')
                 {
                     System.out.print(".");
                 }
@@ -33,8 +117,9 @@ public class MazeTraverse {
                 {
                     System.out.print(" " + maze[i][j]);
                 }
-                System.out.println(); 
+                System.out.println();
             }
+            System.out.println();
         }
     }
 }
